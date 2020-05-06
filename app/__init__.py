c9989cf24dc288934b639a542833a889b2caf4b9
Mary Waithera <njihiamary11@gmail.com>
2020-05-06 15:50:59 +0300

    import and initialize LoginManager to enhance management of the user authentication system

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,11 @@ from flask_bootstrap import Bootstrap
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.signin'
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -20,6 +25,7 @@ def create_app(config_name):
     #extension initialization
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     #blueprint registration
     from .main import main as main_blueprint
