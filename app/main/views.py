bbb4746080d04871044a34ae2e6b533ab1674a3e
Mary Waithera <njihiamary11@gmail.com>
2020-05-07 11:58:13 +0300

    create profile view function and the profile template that this view is to render; add link to profile page to the navbar to ease navigation

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from . import main
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User
 
 @main.route('/')
 def index():
@@ -9,3 +10,16 @@ def index():
     '''
     title='Pomodoro'
     return render_template('index.html',title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+
+    '''
+    function returns profile page and its contents
+    '''
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+    
+    return render_template("profile/profile.html",user=user)
